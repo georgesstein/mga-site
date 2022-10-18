fff61f5dba4930656620462609258dc38ceba154
Georges Markosian <georgesmarkosian@gmail.coma>
2022-10-18 21:38:05 +0400

    Adjust HeadingBlock

---
+++ src/components/HeadingBlock.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+  props: {
+    beforeHeading: {
+      type: String,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    descriptionColor: {
+      type: String,
+      required: false,
+    },
+  },
+
+  computed: {
+    description_style() {
+      const color = this.descriptionColor
+        ? `${this.descriptionColor}`
+        : "#AEAEB2";
+
+      const marginTop = this.description ? "30px" : "0px";
+
+      return {
+        color: color,
+        marginTop: marginTop,
+      };
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="heading">
+    <span>{{ beforeHeading }}</span>
+    <h2>{{ title }}</h2>
+    <p :style="description_style">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.heading {
+  width: 700px;
+  text-align: center;
+
+  & span {
+    @include small-light;
+    color: $second;
+  }
+
+  & h2 {
+    @include large;
+    color: $base;
+  }
+
+  & p {
+    @include small-light;
+    color: $second;
+  }
+}
+</style>
