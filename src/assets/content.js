dd34a006fe50b678830ac59bb1bd9674a32bd59f
Georges Markosian <georgesmarkosian@gmail.coma>
2022-10-21 14:29:13 +0400

    Adjust publishers section

---
+++ src/assets/content.js
@@ -0,0 +1,32 @@
+export const PUBLISHERS = [
+  {
+    title: 'Individual terms of work, include deposit for starting campaign.',
+    logoImage: 'image-01',
+    backgroundImage: 'bg-publishers1',
+  },
+  {
+    title: 'We share our approach for your experience in online marketing.',
+    logoImage: 'image-02',
+    backgroundImage: 'bg-publishers2',
+  },
+  {
+    title: 'Tasty offers in most popular verticals: nutra, gambling, app installs & crypto.',
+    logoImage: 'image-03',
+    backgroundImage: 'bg-publishers3',
+  },
+  {
+    title: 'Worldwide coverage with focus on Tier 1, 2.',
+    logoImage: 'image-04',
+    backgroundImage: 'bg-publishers4',
+  },
+  {
+    title: 'On-time & Flexible Payments.',
+    logoImage: 'image-05',
+    backgroundImage: 'bg-publishers5',
+  },
+  {
+    title: 'Our family will support you 24/7 whenever you need.',
+    logoImage: 'image-06',
+    backgroundImage: 'bg-publishers6',
+  },
+]
--- src/components/PublishersTileItem.vue
+++ src/components/PublishersTileItem.vue
@@ -1,97 +1,83 @@
-
 <script setup>
 import { ref, watchEffect } from "vue";
 
 const props = defineProps({
   backgroundImage: { type: String },
+  logoImage: { type: String },
   title: { type: String },
 });
 
-const logo = ref();
+const icon = ref();
+const image = ref();
 
 watchEffect(async () => {
-  logo.value = (
+  icon.value = (
     await import(
-      /* @vite-ignore */ `../assets/publishers/box/${props.backgroundImage}.png`
+      /* @vite-ignore */ `../assets/publishers/${props.logoImage}.svg`
+    )
+  ).default;
+
+  image.value = (
+    await import(
+      /* @vite-ignore */ `../assets/publishers/box/${props.backgroundImage}.jpg`
     )
   ).default;
 });
-
-const ds = {
-  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url('${logo}')`,
-};
-const hs = {
-  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('${logo}')`,
-};
-</script>
-<script>
-export default {
-  data() {
-    return {
-      isHover: false,
-      bg1: {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url('${logo}')`,
-      },
-      bg2: {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('${logo}')`,
-      },
-    };
-  },
-};
 </script>
 
 <template>
-  <div class="tile">
-    <div class="tile-background" :style="isHover ? bg1 : bg2">
-      <slot></slot>
-      <h3>
-        {{ title }}
-      </h3>
+  <div class="wrapper">
+    <div class="content">
+      <img :src="icon" />
+      <h3>{{ title }}</h3>
     </div>
+
+    <img class="img" :src="image" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.tile {
+.wrapper {
   width: 100%;
-  height: 300px;
-  overflow: hidden;
   position: relative;
+  overflow: hidden;
+  aspect-ratio: 4 / 3;
 
-  &:hover {
-    cursor: url("@/assets/icons/cursor.svg"), auto;
-  }
-
-  &-background {
-    width: 100%;
-    height: 100%;
+  & .content {
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     text-align: center;
-    row-gap: 20px;
-    padding: 30px 50px 50px 50px;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    transition: all 1s;
+    row-gap: 10px;
+    padding: 10%;
+    position: absolute;
+    color: white;
+    z-index: 1;
+    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7));
 
     &:hover {
+      background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1));
+    }
+
+    &:hover + img {
       transform: scale(1.1);
-      background-image: linear-gradient(
-          45deg,
-          rgba(0, 0, 0, 0.1),
-          rgba(0, 0, 0, 0.1)
-        ),
-        url("@/assets/publishers/box/bg-publishers4.png");
+    }
+
+    & h3 {
+      @include medium-bold;
+      font-size: 20px;
+      color: $base;
     }
   }
 
-  & h3 {
-    @include medium-bold;
-    font-size: 20px;
-    color: $base;
+  & img {
+    max-width: 100%;
+    transition: all 1s;
   }
 }
 </style>
